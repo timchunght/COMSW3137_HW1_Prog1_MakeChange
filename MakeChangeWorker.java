c919ee843eb504e9240dc9fff3932578afba4a8c
timchunght <timchunght@gmail.com>
2015-02-03 00:36:52 -0500

    updated print

--- MakeChangeWorker.java
+++ MakeChangeWorker.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
-public class MakeChangeWorker {
+public class MakeChangeWorker 
+{
 
 	public MakeChangeWorker() 
 	{
@@ -12,26 +13,27 @@ public class MakeChangeWorker {
 			ArrayList<int[]> combinations, int index) 
 	{
 		int[] VALUES = { 5, 10, 25 };
-
+		
+		//return the an empty AL of size 0 if amount is not divisible by 5
 		if (amount % 5 != 0)
 		{
 			return combinations;
 		}
 
-		
+		//if amount is 0, we should have reached a combo, so add array/combo to AL
 		if (amount == 0) 
 		{
 			combinations.add(coinCounts);
 			
 		}
 
-		
+		//Start from the given index, 0 is the index for nickel, 1 for dime, and 2 for quarter
 		for (int i = index; i < VALUES.length; i++) 
 		{
 			
 			if (VALUES[i] <= amount) 
 			{
-				
+				//prepare a duplicate coinCounts array for recursion
 				int[] newCoinCounts = (int[]) coinCounts.clone();
 				newCoinCounts[i]++;
 
@@ -47,6 +49,7 @@ public class MakeChangeWorker {
 	{
 		int[] VALUES = { 5, 10, 25 };
 		
+		ArrayList<String> output = new ArrayList<String>();
 		if (combinations.size() == 0) 
 		{
 			System.out.println(amount + " can't be changed.");
@@ -55,15 +58,20 @@ public class MakeChangeWorker {
 		
 		for (int s = 0; s < combinations.size(); s++) 
 		{
-			System.out.print("Change for " + amount + " = ");
+			String header = "Change for " + amount + " = ";
 			for (int i = 0; i < VALUES.length; i++) 
 			{
 				for (int j = 0; j < combinations.get(s)[i]; j++) 
 				{
-					System.out.print(VALUES[i] + " ");
+					header+=(VALUES[i] + " ");
 				}
 			}
-			System.out.println();
+			output.add(header);
+		}
+		
+		for( String s : output)
+		{
+			System.out.println(s);
 		}
 	}
 
