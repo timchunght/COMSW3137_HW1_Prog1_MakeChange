d1a4bc7dfabc0a3c49183f2c6188a5be64657bcf
timchunght <timchunght@gmail.com>
2015-02-03 16:37:59 -0500

    spacing cleaned

--- MakeChange.java
+++ MakeChange.java
@@ -1,18 +1,9 @@
-import java.util.ArrayList;
-
-
 public class MakeChange 
 {
    public static void main (String[] args) 
     {
-       
-	
 	    int amount = Integer.parseInt(args[0]);
-	  
-	    
-
 	    MakeChangeWorker makeChange = new MakeChangeWorker(amount);
-	    
 	    makeChange.findCombinations(amount, 0, new int[3]);
 	
     }
