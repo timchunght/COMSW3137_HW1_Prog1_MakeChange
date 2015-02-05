783fe115241f7ad5d07075cae89adff25c27db21
timchunght <timchunght@gmail.com>
2015-02-05 05:57:45 -0500

    added build script and updated readme

---
+++ build.sh
@@ -0,0 +1,9 @@
+echo "Compiling now: javac MakeChange.java"
+javac MakeChange.java
+echo "Running Some Testing Cases now:"
+echo "java MakeChange 20 (Change the number if you want to run a different test case)"
+java MakeChange 20
+echo "java MakeChange 33"
+java MakeChange 33
+echo "java MakeChange 50"
+java MakeChange 50
--- readme.md
+++ readme.md
@@ -1,2 +1,9 @@
 README
 ---
+
+The run/build script is all in build.sh, so to run it, run:
+
+```bash
+sh build.sh
+```
+That's all you need to do! The buildscript will compile and run some sample test cases. 
